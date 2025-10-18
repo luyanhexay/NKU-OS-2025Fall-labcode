41cc38c6bfd7ac77546c1111be57253f2c0a2324
u2003yuge <3298809085@qq.com>
2025-10-18 20:41:23 +0800

    test buddy

--- labcode/lab2/kern/mm/pmm.c
+++ labcode/lab2/kern/mm/pmm.c
@@ -1,5 +1,6 @@
 #include <default_pmm.h>
 #include <best_fit_pmm.h>
+#include <buddy_pmm.h>
 #include <defs.h>
 #include <error.h>
 #include <memlayout.h>
@@ -34,7 +35,7 @@ static void check_alloc_page(void);
 
 // init_pmm_manager - initialize a pmm_manager instance
 static void init_pmm_manager(void) {
-    pmm_manager = &default_pmm_manager;
+    pmm_manager = &buddy_pmm_manager;
     cprintf("memory management: %s\n", pmm_manager->name);
     pmm_manager->init();
 }
@@ -84,7 +85,7 @@ static void page_init(void) {
     extern char end[];
 
     npage = maxpa / PGSIZE;
-    //kernel在end[]结束, pages是剩下的页的开始
+    //kernel在end[]结束, pages是剩下的页的开�?
     pages = (struct Page *)ROUNDUP((void *)end, PGSIZE);
 
     for (size_t i = 0; i < npage - nbase; i++) {
