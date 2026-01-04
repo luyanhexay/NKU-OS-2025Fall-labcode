07b4b0d895cb2c14e74a9ca6347e9931d1a8ce58
KamiuKamew <KamiuKamew@github.com>
2026-01-04 23:56:13 +0800

    feat(lab6): finish exercises 0-2
    Passes \make: Entering directory '/home/hexay/projects/LabProj/OS/labcode/lab6'
    make: Leaving directory '/home/hexay/projects/LabProj/OS/labcode/lab6' in this environment.

--- labcode/lab6/kern/trap/trap.c
+++ labcode/lab6/kern/trap/trap.c
@@ -122,16 +122,12 @@ void interrupt_handler(struct trapframe *tf)
         // directly.
         // clear_csr(sip, SIP_STIP);
 
-        /* LAB3 :填写你在lab3中实现的代码 */
-        /*(1)设置下次时钟中断- clock_set_next_event()
-         *(2)计数器（ticks）加一
-         *(3)当计数器加到100的时候，我们会输出一个`100ticks`表示我们触发了100次时钟中断，同时打印次数（num）加一
-         * (4)判断打印次数，当打印次数为10时，调用<sbi.h>中的关机函数关机
-         */
-
-        // lab6: YOUR CODE  (update LAB3 steps)
-        //  在时钟中断时调用调度器的 sched_class_proc_tick 函数
-
+        clock_set_next_event();
+        ++ticks;
+        if (current)
+        {
+            sched_class_proc_tick(current);
+        }
         break;
     case IRQ_H_TIMER:
         cprintf("Hypervisor software interrupt\n");
